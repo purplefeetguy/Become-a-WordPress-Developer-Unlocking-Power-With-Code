0ebcd190634ecd2bc78a9ee79bf93e9d8d72f7bd
purplefeetguy <purplefeetguy@hotmail.com>
2020-11-24 05:55:43 -0600

    simple loop through posts using WP functions

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 
-  $names = array('Brad', 'John', 'Jane', 'Meowsalot', 'Barksalot');
-
-  $count = 0;
-
-  while($count < count($names)) {
-      echo "<li>Hi, my name is $names[$count]</li>";
-      $count++;
-  }
+  while(have_posts()){
+      the_post(); ?>
+      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <?php the_content(); ?>
+      <hr>
+  <?php }
 
 ?>
