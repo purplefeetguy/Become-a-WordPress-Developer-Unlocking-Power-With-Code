0f7b6c060e671bab08c73d49d451c29e293000dd
purplefeetguy <purplefeetguy@hotmail.com>
2020-11-24 05:27:47 -0600

    basic while loop

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 
   $names = array('Brad', 'John', 'Jane', 'Meowsalot');
 
+  $count = 1;
+
+  while($count < 101) {
+      echo "<li>$count</li>";
+      $count++;
+  }
+
 ?>
 
 <p>Hi, my name is <?php echo $names[0] ?></p>
